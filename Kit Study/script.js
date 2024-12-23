c35d53101fd079d97446a045f39b54a8db8275e2
Craftyman6 <schaffner.colindavid@gmail.com>
2024-12-23 01:07:18 -0500

    Implement weapons js file

--- Kit Study/script.js
+++ Kit Study/script.js
@@ -1,1 +1,5 @@
+import { weapons } from './weapons.js';
+
+console.log(weapons[1].name);
+
 document.getElementById("title").textContent="Sample Text";
---
+++ Kit Study/weapons.js
@@ -0,0 +1,717 @@
+export const weapons = [
+
+	{name: ".52 Gal",
+	sub: "Splash Wall",
+	special: "Killer Wail 5.1",
+	wclass: "Shooter"},
+
+	{name: ".52 Gal Deco",
+	sub: "Curling Bomb",
+	special: "Splattercolor Screen",
+	wclass: "Shooter"},
+
+	{name: ".96 Gal",
+	sub: "Sprinkler",
+	special: "Ink Vac",
+	wclass: "Shooter"},
+
+	{name: ".96 Gal Deco",
+	sub: "Splash Wall",
+	special: "Kraken Royale",
+	wclass: "Shooter"},
+
+	{name: "Aerospray MG",
+	sub: "Fizzy Bomb",
+	special: "Reefslider",
+	wclass: "Shooter"},
+
+	{name: "Aerospray RG",
+	sub: "Sprinkler",
+	special: "Booyah Bomb",
+	wclass: "Shooter"},
+
+	{name: "Annaki Splattershot Nova",
+	sub: "Ink Mine",
+	special: "Inkjet",
+	wclass: "Shooter"},
+
+	{name: "Ballpoint Splatling",
+	sub: "Fizzy Bomb",
+	special: "Inkjet",
+	wclass: "Splatling"},
+
+	{name: "Ballpoint Splatling Nouveau",
+	sub: "Ink Mine",
+	special: "Ink Vac",
+	wclass: "Splatling"},
+
+	{name: "Bamboozler 14 Mk I",
+	sub: "Autobomb",
+	special: "Killer Wail 5.1",
+	wclass: "Charger"},
+
+	{name: "Bamboozler 14 Mk II",
+	sub: "Fizzy Bomb",
+	special: "Super Chump",
+	wclass: "Charger"},
+
+	{name: "Big Swig Roller",
+	sub: "Splash Wall",
+	special: "Ink Vac",
+	wclass: "Roller"},
+
+	{name: "Big Swig Roller Express",
+	sub: "Angle Shooter",
+	special: "Ink Storm",
+	wclass: "Roller"},
+
+	{name: "Blaster",
+	sub: "Autobomb",
+	special: "Big Bubbler",
+	wclass: "Blaster"},
+
+	{name: "Bloblobber",
+	sub: "Sprinkler",
+	special: "Ink Storm",
+	wclass: "Slosher"},
+
+	{name: "Bloblobber Deco",
+	sub: "Angle Shooter",
+	special: "Kraken Royale",
+	wclass: "Slosher"},
+
+	{name: "Carbon Roller",
+	sub: "Autobomb",
+	special: "Zipcaster",
+	wclass: "Roller"},
+
+	{name: "Carbon Roller Deco",
+	sub: "Burst Bomb",
+	special: "Trizooka",
+	wclass: "Roller"},
+
+	{name: "Charcoal Decavitator",
+	sub: "Splash Wall",
+	special: "Inkjet",
+	wclass: "Splatana"},
+
+	{name: "Clash Blaster",
+	sub: "Splat Bomb",
+	special: "Trizooka",
+	wclass: "Blaster"},
+
+	{name: "Clash Blaster Neo",
+	sub: "Curling Bomb",
+	special: "Super Chump",
+	wclass: "Blaster"},
+
+	{name: "Classic Squiffer",
+	sub: "Point Sensor",
+	special: "Big Bubbler",
+	wclass: "Charger"},
+
+	{name: "Custom Blaster",
+	sub: "Point Sensor",
+	special: "Triple Splashdown",
+	wclass: "Blaster"},
+
+	{name: "Custom Douser Dualies FF",
+	sub: "Burst Bomb",
+	special: "Triple Inkstrike",
+	wclass: "Dualie"},
+
+	{name: "Custom Dualie Squelchers",
+	sub: "Squid Beakon",
+	special: "Super Chump",
+	wclass: "Dualie"},
+
+	{name: "Custom E-liter 4K",
+	sub: "Squid Beakon",
+	special: "Kraken Royale",
+	wclass: "Charger"},
+
+	{name: "Custom E-liter 4K Scope",
+	sub: "Squid Beakon",
+	special: "Kraken Royale",
+	wclass: "Charger"},
+
+	{name: "Custom Explosher",
+	sub: "Splash Wall",
+	special: "Triple Splashdown",
+	wclass: "Slosher"},
+
+	{name: "Custom Goo Tuber",
+	sub: "Fizzy Bomb",
+	special: "Ultra Stamp",
+	wclass: "Charger"},
+
+	{name: "Custom Hydra Splatling",
+	sub: "Ink Mine",
+	special: "Splattercolor Screen",
+	wclass: "Hydra"},
+
+	{name: "Custom Jet Squelcher",
+	sub: "Toxic Mist",
+	special: "Ink Storm",
+	wclass: "Shooter"},
+
+	{name: "Custom Range Blaster",
+	sub: "Splat Bomb",
+	special: "Kraken Royale",
+	wclass: "Blaster"},
+
+	{name: "Custom Splattershot Jr.",
+	sub: "Torpedo",
+	special: "Wave Breaker",
+	wclass: "Shooter"},
+
+	{name: "Custom Wellstring V",
+	sub: "Point Sensor",
+	special: "Wave Breaker",
+	wclass: "Stringer"},
+
+	{name: "Dapple Dualies",
+	sub: "Squid Beakon",
+	special: "Tacticooler",
+	wclass: "Dualie"},
+
+	{name: "Dapple Dualies Nouveau",
+	sub: "Torpedo",
+	special: "Reefslider",
+	wclass: "Dualie"},
+
+	{name: "Dark Tetra Dualies",
+	sub: "Autobomb",
+	special: "Reefslider",
+	wclass: "Dualie"},
+
+	{name: "Douser Dualies FF",
+	sub: "Ink Mine",
+	special: "Killer Wail 5.1",
+	wclass: "Dualie"},
+
+	{name: "Dread Wringer",
+	sub: "Suction Bomb",
+	special: "Reefslider",
+	wclass: "Slosher"},
+
+	{name: "Dread Wringer D",
+	sub: "Squid Beakon",
+	special: "Wave Breaker",
+	wclass: "Slosher"},
+
+	{name: "Dualie Squelchers",
+	sub: "Splat Bomb",
+	special: "Wave Breaker",
+	wclass: "Dualie"},
+
+	{name: "Dynamo Roller",
+	sub: "Sprinkler",
+	special: "Tacticooler",
+	wclass: "Roller"},
+
+	{name: "E-liter 4K",
+	sub: "Ink Mine",
+	special: "Wave Breaker",
+	wclass: "Charger"},
+
+	{name: "E-liter 4K Scope",
+	sub: "Ink Mine",
+	special: "Wave Breaker",
+	wclass: "Charger"},
+
+	{name: "Enperry Splat Dualies",
+	sub: "Curling Bomb",
+	special: "Triple Splashdown",
+	wclass: "Dualie"},
+
+	{name: "Explosher",
+	sub: "Point Sensor",
+	special: "Ink Storm",
+	wclass: "Slosher"},
+
+	{name: "Flingza Roller",
+	sub: "Ink Mine",
+	special: "Tenta Missiles",
+	wclass: "Roller"},
+
+	{name: "Foil Flingza Roller",
+	sub: "Suction Bomb",
+	special: "Splattercolor Screen",
+	wclass: "Roller"},
+
+	{name: "Foil Squeezer",
+	sub: "Autobomb",
+	special: "Splattercolor Screen",
+	wclass: "Shooter"},
+
+	{name: "Forge Splattershot Pro",
+	sub: "Suction Bomb",
+	special: "Booyah Bomb",
+	wclass: "Shooter"},
+
+	{name: "Glooga Dualies",
+	sub: "Splash Wall",
+	special: "Booyah Bomb",
+	wclass: "Dualie"},
+
+	{name: "Glooga Dualies Deco",
+	sub: "Point Sensor",
+	special: "Trizooka",
+	wclass: "Dualie"},
+
+	{name: "Goo Tuber",
+	sub: "Torpedo",
+	special: "Tenta Missiles",
+	wclass: "Charger"},
+
+	{name: "Gold Dynamo Roller",
+	sub: "Splat Bomb",
+	special: "Super Chump",
+	wclass: "Roller"},
+
+	{name: "H-3 Nozzlenose",
+	sub: "Point Sensor",
+	special: "Tacticooler",
+	wclass: "Shooter"},
+
+	{name: "H-3 Nozzlenose D",
+	sub: "Splash Wall",
+	special: "Big Bubbler",
+	wclass: "Shooter"},
+
+	{name: "Heavy Edit Splatling",
+	sub: "Curling Bomb",
+	special: "Tacticooler",
+	wclass: "Splatling"},
+
+	{name: "Heavy Edit Splatling Nouveau",
+	sub: "Splat Bomb",
+	special: "Crab Tank",
+	wclass: "Splatling"},
+
+	{name: "Heavy Splatling",
+	sub: "Sprinkler",
+	special: "Wave Breaker",
+	wclass: "Splatling"},
+
+	{name: "Heavy Splatling Deco",
+	sub: "Point Sensor",
+	special: "Kraken Royale",
+	wclass: "Splatling"},
+
+	{name: "Hero Shot Replica",
+	sub: "Suction Bomb",
+	special: "Trizooka",
+	wclass: "Shooter"},
+
+	{name: "Hydra Splatling",
+	sub: "Autobomb",
+	special: "Booyah Bomb",
+	wclass: "Splatling"},
+
+	{name: "Inkbrush",
+	sub: "Splat Bomb",
+	special: "Killer Wail 5.1",
+	wclass: "Brush"},
+
+	{name: "Inkbrush Nouveau",
+	sub: "Ink Mine",
+	special: "Ultra Stamp",
+	wclass: "Brush"},
+
+	{name: "Inkline Tri-Stringer",
+	sub: "Sprinkler",
+	special: "Super Chump",
+	wclass: "Stringer"},
+
+	{name: "Jet Squelcher",
+	sub: "Angle Shooter",
+	special: "Ink Vac",
+	wclass: "Shooter"},
+
+	{name: "Krak-On Splat Roller",
+	sub: "Squid Beakon",
+	special: "Kraken Royale",
+	wclass: "Roller"},
+
+	{name: "L-3 Nozzlenose",
+	sub: "Curling Bomb",
+	special: "Crab Tank",
+	wclass: "Shooter"},
+
+	{name: "L-3 Nozzlenose D",
+	sub: "Burst Bomb",
+	special: "Ultra Stamp",
+	wclass: "Shooter"},
+
+	{name: "Light Tetra Dualies",
+	sub: "Sprinkler",
+	special: "Zipcaster",
+	wclass: "Dualie"},
+
+	{name: "Luna Blaster",
+	sub: "Splat Bomb",
+	special: "Zipcaster",
+	wclass: "Blaster"},
+
+	{name: "Luna Blaster Neo",
+	sub: "Fizzy Bomb",
+	special: "Ultra Stamp",
+	wclass: "Blaster"},
+
+	{name: "Mini Splatling",
+	sub: "Burst Bomb",
+	special: "Ultra Stamp",
+	wclass: "Splatling"},
+
+	{name: "Mint Decavitator",
+	sub: "Suction Bomb",
+	special: "Big Bubbler",
+	wclass: "Splatana"},
+
+	{name: "N-ZAP '85",
+	sub: "Suction Bomb",
+	special: "Tacticooler",
+	wclass: "Shooter"},
+
+	{name: "N-ZAP '89",
+	sub: "Autobomb",
+	special: "Super Chump",
+	wclass: "Shooter"},
+
+	{name: "Nautilus 47",
+	sub: "Point Sensor",
+	special: "Ink Storm",
+	wclass: "Splatling"},
+
+	{name: "Nautilus 79",
+	sub: "Suction Bomb",
+	special: "Triple Splashdown",
+	wclass: "Splatling"},
+
+	{name: "Neo Splash-o-matic",
+	sub: "Suction Bomb",
+	special: "Triple Inkstrike",
+	wclass: "Shooter"},
+
+	{name: "Neo Sploosh-o-matic",
+	sub: "Squid Beakon",
+	special: "Killer Wail 5.1",
+	wclass: "Shooter"},
+
+	{name: "New Squiffer",
+	sub: "Autobomb",
+	special: "Zipcaster",
+	wclass: "Charger"},
+
+	{name: "Octo Shot Replica",
+	sub: "Splat Bomb",
+	special: "Triple Inkstrike",
+	wclass: "Shooter"},
+
+	{name: "Octobrush",
+	sub: "Suction Bomb",
+	special: "Zipcaster",
+	wclass: "Brush"},
+
+	{name: "Octobrush Nouveau",
+	sub: "Squid Beakon",
+	special: "Ink Storm",
+	wclass: "Brush"},
+
+	{name: "Order Blaster Replica",
+	sub: "Splat Bomb",
+	special: "Zipcaster",
+	wclass: "Blaster"},
+
+	{name: "Order Brella Replica",
+	sub: "Sprinkler",
+	special: "Triple Inkstrike",
+	wclass: "Brella"},
+
+	{name: "Order Charger Replica",
+	sub: "Splat Bomb",
+	special: "Ink Vac",
+	wclass: "Charger"},
+
+	{name: "Order Dualie Replicas",
+	sub: "Suction Bomb",
+	special: "Crab Tank",
+	wclass: "Dualie"},
+
+	{name: "Order Roller Replica",
+	sub: "Curling Bomb",
+	special: "Big Bubbler",
+	wclass: "Roller"},
+
+	{name: "Order Shot Replica",
+	sub: "Suction Bomb",
+	special: "Trizooka",
+	wclass: "Shooter"},
+
+	{name: "Order Slosher Replica",
+	sub: "Splat Bomb",
+	special: "Triple Inkstrike",
+	wclass: "Slosher"},
+
+	{name: "Order Splatana Replica",
+	sub: "Burst Bomb",
+	special: "Zipcaster",
+	wclass: "Splatana"},
+
+	{name: "Order Splatling Replica",
+	sub: "Sprinkler",
+	special: "Wave Breaker",
+	wclass: "Splatling"},
+
+	{name: "Order Stringer Replica",
+	sub: "Toxic Mist",
+	special: "Killer Wail 5.1",
+	wclass: "Stringer"},
+
+	{name: "Orderbrush Replica",
+	sub: "Suction Bomb",
+	special: "Zipcaster",
+	wclass: "Brush"},
+
+	{name: "Painbrush",
+	sub: "Curling Bomb",
+	special: "Wave Breaker",
+	wclass: "Brush"},
+
+	{name: "Painbrush Nouveau",
+	sub: "Point Sensor",
+	special: "Tenta Missiles",
+	wclass: "Brush"},
+
+	{name: "Range Blaster",
+	sub: "Suction Bomb",
+	special: "Wave Breaker",
+	wclass: "Blaster"},
+
+	{name: "Rapid Blaster",
+	sub: "Ink Mine",
+	special: "Triple Inkstrike",
+	wclass: "Blaster"},
+
+	{name: "Rapid Blaster Deco",
+	sub: "Torpedo",
+	special: "Inkjet",
+	wclass: "Blaster"},
+
+	{name: "Rapid Blaster Pro",
+	sub: "Toxic Mist",
+	special: "Ink Vac",
+	wclass: "Blaster"},
+
+	{name: "Rapid Blaster Pro Deco",
+	sub: "Angle Shooter",
+	special: "Killer Wail 5.1",
+	wclass: "Blaster"},
+
+	{name: "Recycled Brella 24 Mk I",
+	sub: "Angle Shooter",
+	special: "Big Bubbler",
+	wclass: "Brella"},
+
+	{name: "Recycled Brella 24 Mk II",
+	sub: "Toxic Mist",
+	special: "Triple Splashdown",
+	wclass: "Brella"},
+
+	{name: "REEF-LUX 450",
+	sub: "Curling Bomb",
+	special: "Tenta Missiles",
+	wclass: "Stringer"},
+
+	{name: "REEF-LUX 450 Deco",
+	sub: "Splash Wall",
+	special: "Reefslider",
+	wclass: "Stringer"},
+
+	{name: "S-BLAST '91",
+	sub: "Burst Bomb",
+	special: "Booyah Bomb",
+	wclass: "Blaster"},
+
+	{name: "S-BLAST '92",
+	sub: "Sprinkler",
+	special: "Reefslider",
+	wclass: "Blaster"},
+
+	{name: "Slosher",
+	sub: "Splat Bomb",
+	special: "Triple Inkstrike",
+	wclass: "Slosher"},
+
+	{name: "Slosher Deco",
+	sub: "Angle Shooter",
+	special: "Zipcaster",
+	wclass: "Slosher"},
+
+	{name: "Sloshing Machine",
+	sub: "Fizzy Bomb",
+	special: "Booyah Bomb",
+	wclass: "Slosher"},
+
+	{name: "Sloshing Machine Neo",
+	sub: "Point Sensor",
+	special: "Trizooka",
+	wclass: "Slosher"},
+
+	{name: "Snipewriter 5B",
+	sub: "Splash Wall",
+	special: "Ink Storm",
+	wclass: "Charger"},
+
+	{name: "Snipewriter 5H",
+	sub: "Sprinkler",
+	special: "Tacticooler",
+	wclass: "Charger"},
+
+	{name: "Sorella Brella",
+	sub: "Autobomb",
+	special: "Inkjet",
+	wclass: "Brella"},
+
+	{name: "Splash-o-matic",
+	sub: "Burst Bomb",
+	special: "Crab Tank",
+	wclass: "Shooter"},
+
+	{name: "Splat Brella",
+	sub: "Sprinkler",
+	special: "Triple Inkstrike",
+	wclass: "Brella"},
+
+	{name: "Splat Charger",
+	sub: "Splat Bomb",
+	special: "Ink Vac",
+	wclass: "Charger"},
+
+	{name: "Splat Dualies",
+	sub: "Suction Bomb",
+	special: "Crab Tank",
+	wclass: "Dualie"},
+
+	{name: "Splat Roller",
+	sub: "Curling Bomb",
+	special: "Big Bubbler",
+	wclass: "Roller"},
+
+	{name: "Splatana Stamper",
+	sub: "Burst Bomb",
+	special: "Zipcaster",
+	wclass: "Splatana"},
+
+	{name: "Splatana Stamper Nouveau",
+	sub: "Toxic Mist",
+	special: "Crab Tank",
+	wclass: "Splatana"},
+
+	{name: "Splatana Wiper",
+	sub: "Torpedo",
+	special: "Ultra Stamp",
+	wclass: "Splatana"},
+
+	{name: "Splatana Wiper Deco",
+	sub: "Squid Beakon",
+	special: "Tenta Missiles",
+	wclass: "Splatana"},
+
+	{name: "Splatterscope",
+	sub: "Splat Bomb",
+	special: "Ink Vac",
+	wclass: "Charger"},
+
+	{name: "Splattershot",
+	sub: "Suction Bomb",
+	special: "Trizooka",
+	wclass: "Shooter"},
+
+	{name: "Splattershot Jr.",
+	sub: "Splat Bomb",
+	special: "Big Bubbler",
+	wclass: "Shooter"},
+
+	{name: "Splattershot Nova",
+	sub: "Point Sensor",
+	special: "Killer Wail 5.1",
+	wclass: "Shooter"},
+
+	{name: "Splattershot Pro",
+	sub: "Angle Shooter",
+	special: "Crab Tank",
+	wclass: "Shooter"},
+
+	{name: "Sploosh-o-matic",
+	sub: "Curling Bomb",
+	special: "Ultra Stamp",
+	wclass: "Shooter"},
+
+	{name: "Squeezer",
+	sub: "Splash Wall",
+	special: "Trizooka",
+	wclass: "Shooter"},
+
+	{name: "Tenta Brella",
+	sub: "Squid Beakon",
+	special: "Ink Vac",
+	wclass: "Brella"},
+
+	{name: "Tenta Sorella Brella",
+	sub: "Ink Mine",
+	special: "Trizooka",
+	wclass: "Brella"},
+
+	{name: "Tentatek Splattershot",
+	sub: "Splat Bomb",
+	special: "Triple Inkstrike",
+	wclass: "Shooter"},
+
+	{name: "Tri-Slosher",
+	sub: "Toxic Mist",
+	special: "Inkjet",
+	wclass: "Slosher"},
+
+	{name: "Tri-Slosher Nouveau",
+	sub: "Fizzy Bomb",
+	special: "Tacticooler",
+	wclass: "Slosher"},
+
+	{name: "Tri-Stringer",
+	sub: "Toxic Mist",
+	special: "Killer Wail 5.1",
+	wclass: "Stringer"},
+
+	{name: "Undercover Brella",
+	sub: "Ink Mine",
+	special: "Reefslider",
+	wclass: "Brella"},
+
+	{name: "Undercover Sorella Brella",
+	sub: "Torpedo",
+	special: "Splattercolor Screen",
+	wclass: "Brella"},
+
+	{name: "Wellstring V",
+	sub: "Autobomb",
+	special: "Ultra Stamp",
+	wclass: "Stringer"},
+
+	{name: "Z+F Splat Charger",
+	sub: "Splash Wall",
+	special: "Triple Inkstrike",
+	wclass: "Charger"},
+
+	{name: "Z+F Splatterscope",
+	sub: "Splash Wall",
+	special: "Triple Inkstrike",
+	wclass: "Charger"},
+
+	{name: "Zink Mini Splatling",
+	sub: "Toxic Mist",
+	special: "Big Bubbler",
+	wclass: "Splatling"}
+];
